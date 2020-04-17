403806ebae47cb88309f5f7a0da5fe7bec885395
ico10297024 <ico10297024@vip.qq.com>
2020-04-17 14:34:16 +0800

    修改了协议

--- sample/src/main/java/ico/ico/ble/demo/blemgr/BleMgr.java
+++ sample/src/main/java/ico/ico/ble/demo/blemgr/BleMgr.java
@@ -27,7 +27,7 @@ public class BleMgr {
     //当前操作标识
     CurrentOperationFlag currentOperationFlag = new CurrentOperationFlag();
     //超时时间
-    private long TIMESOUT = 5 * 1000L;
+    public static final long TIMESOUT = 10 * 1000L;
     //蓝牙连接器
     private BleSocket mBleSocket;
     /**
@@ -43,59 +43,6 @@ public class BleMgr {
         this.mBleCallback = bleCallback;
     }
 
-    /**
-     * view层接收到数据回调,调用这个函数去分析收到的数据,对数据进行校验和处理,然后返回这条指令的动作标识
-     * TODO 根据实际的项目协议进行改写
-     *
-     * @param data 接收到的数据
-     * @return
-     */
-    /*public byte analyze(byte[] data) {
-        if (data.length < 14) {
-            return -1;
-        }
-        if ((data[0] & 0xff) == 0xf5 &&
-                (data[1] & 0xff) == 0xf5 &&
-                (data[4] & 0xff) == 0x0d &&
-                (data[5] & 0xff) == 0x0a
-                ) {
-            currentOperationFlag.finishOper(data[2]);
-            switch (data[2]) {
-                case Command.CMD_OPEN:
-                    break;
-                case Command.CMD_QUERY_POWER:
-                    power = data[3];
-                    break;
-            }
-            return data[2];
-        } else {
-            return -1;
-        }
-    }*/
-
-    /** TODO 这里对接收到的数据进行处理，根据具体的协议进行修改，返回具体的操作标识 */
-    public byte analyze(byte[] buffer) {
-        if (buffer.length < 19) {
-            return -1;
-        }
-        if ((buffer[0] & 0xff) == 0xf5
-                && (buffer[1] & 0xff) == 0xf5
-                && (buffer[17] & 0xff) == 0x0d
-                && (buffer[18] & 0xff) == 0x0a
-                ) {
-            currentOperationFlag.finishOper(buffer[3]);
-            //TODO 在这里根据不同的操作，做成功处理
-            switch (buffer[3]) {
-                case Command.CMD_OPEN:
-                    //开门成功了
-                    break;
-            }
-            return buffer[3];//返回本次接收到的数据代表什么操作
-        } else {
-            return -1;
-        }
-    }
-
     //region Control
 
     /** 开门控制，filter设置筛选关键字，type设置筛选类型，{@link BleSocket#setBleFilterCondition(String, int)} */
@@ -107,7 +54,7 @@ public class BleMgr {
             setBleSocket(bleSocket);
         }
         //通过发送来启动流程
-        mBleSocket.send(Command.getCommon(Command.CMD_OPEN));
+        mBleSocket.send(Command.getNewCommon(Command.CMD_OPEN));
         Subscription sub = Observable.just("").subscribeOn(AndroidSchedulers.mainThread()).delay(TIMESOUT, TimeUnit.MILLISECONDS).subscribe(action1);
         currentOperationFlag.saveOpering(Command.CMD_OPEN, sub);
     }
@@ -206,6 +153,45 @@ public class BleMgr {
             data[18] = 0x0A;
 
             return data;
+        }
+
+        public static byte[] getNewCommon(byte cmd) {
+            byte[] data = new byte[6];
+            //校验位
+            data[0] = (byte) 0xf5;
+            data[1] = (byte) 0xf5;
+            //类型
+            data[2] = (byte) 1;
+            //操作类型 1开门， 2复位， 3开授权， 4关授权， 5查询列表， 6删除指定权限， 7删除全部权限 8 授权主动返回 9查询电量
+            data[3] = cmd;
+            //校验位
+            data[4] = 0x0D;
+            data[5] = 0x0A;
+
+            return data;
+        }
+    }
+
+    /** TODO 这里对接收到的数据进行处理，根据具体的协议进行修改，返回具体的操作标识 */
+    public byte analyze(byte[] buffer) {
+        if (buffer.length < 6) {
+            return -1;
+        }
+        if ((buffer[0] & 0xff) == 0xf5
+                && (buffer[1] & 0xff) == 0xf5
+                && (buffer[4] & 0xff) == 0x0d
+                && (buffer[5] & 0xff) == 0x0a
+                ) {
+            currentOperationFlag.finishOper(buffer[3]);
+            //TODO 在这里根据不同的操作，做成功处理
+            switch (buffer[3]) {
+                case Command.CMD_OPEN:
+                    //开门成功了
+                    break;
+            }
+            return buffer[3];//返回本次接收到的数据代表什么操作
+        } else {
+            return -1;
         }
     }
 }
